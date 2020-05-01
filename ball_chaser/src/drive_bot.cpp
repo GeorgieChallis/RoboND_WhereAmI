65ea41fcfa51f74624f23d5f28971c4e047f2326
GeorgieChallis <g.l.challis@student.reading.ac.uk>
2020-05-01 04:25:17 -0700

    feat: Added command_robot callback function

--- ball_chaser/src/drive_bot.cpp
+++ ball_chaser/src/drive_bot.cpp
@@ -1,20 +1,33 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "ball_chaser/DriveToTarget.h"
-#include <std_msgs/Float64.h>
 
 // ROS::Publisher motor commands;
 ros::Publisher motor_command_publisher;
 
-// TODO: Create a handle_drive_request callback function that executes whenever a drive_bot service is requested
-// This function should publish the requested linear x and angular velocities to the robot wheel joints
-// After publishing the requested velocities, a message feedback should be returned with the requested wheel velocities
-
 // This callback function executes whenever a drive_bot service is requested
-bool handle_drive_bot_request(ball_chaser::DriveToTarget::Request& req,
+bool handle_command_robot_request(ball_chaser::DriveToTarget::Request& req,
     ball_chaser::DriveToTarget::Response& res)
 {
   ROS_INFO("DriveToTargetRequest received - j1:%1.2f, j2:%1.2f", (float)req.linear_x, (float)req.angular_z);
+
+  float linearx_input = req.linear_x;
+  float angularz_input = req.angular_z;
+
+  // Create a motor_command object of type geometry_msgs::Twist
+  geometry_msgs::Twist motor_command;
+  // Set wheel velocities
+  motor_command.linear.x = linearx_input;
+  motor_command.angular.z = angularz_input;
+  // Publish angles to drive the robot
+  motor_command_publisher.publish(motor_command);
+
+   // Wait 3 seconds to settle
+   ros::Duration(3).sleep();
+
+    // Return a response message
+    res.msg_feedback = "Robot velocities set: " + std::to_string(linearx_input) + ", " + std::to_string(angularz_input);
+    ROS_INFO_STREAM(res.msg_feedback);
 
   return true;
 }
@@ -31,21 +44,14 @@ int main(int argc, char** argv)
     // Inform ROS master that we will be publishing a message of type geometry_msgs::Twist on the robot actuation topic with a publishing queue size of 10
     motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
-    // TODO: Define a drive /ball_chaser/command_robot service with a handle_drive_request callback function
+    // Define a drive /ball_chaser/command_robot service with a handle_drive_request callback function
+    ros::ServiceServer service = n.advertiseService("ball_chaser/command_robot", handle_command_robot_request);
 
-    // TODO: Delete the loop, move the code to the inside of the callback function and make the necessary changes to publish the requested velocities instead of constant values
-    while (ros::ok()) {
-        // Create a motor_command object of type geometry_msgs::Twist
-        geometry_msgs::Twist motor_command;
-        // Set wheel velocities, forward [0.5, 0.0]
-        motor_command.linear.x = 0.5;
-        motor_command.angular.z = 0.0;
-        // Publish angles to drive the robot
-        motor_command_publisher.publish(motor_command);
-    }
+    ROS_INFO("Ready to send robot velocity commands");
 
-    // TODO: Handle ROS communication events
-    //ros::spin();
+
+    // Handle ROS communication events
+    ros::spin();
 
     return 0;
 }
