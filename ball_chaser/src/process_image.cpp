83da5086056d267fafbf9c8092f9982de2645504
GeorgieChallis <g.l.challis@student.reading.ac.uk>
2020-05-03 11:28:18 +0000

    feat: Implement full ball-following functions

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -5,12 +5,12 @@
 // Define a global client that can request services
 ros::ServiceClient client;
 
+static bool moving = false;
+
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
     // Request a service and pass the velocities to it to drive the robot
-    ROS_INFO_STREAM("Driving robot...");
-
     // Request velocities
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
@@ -25,41 +25,36 @@ void drive_robot(float lin_x, float ang_z)
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
-    
-    int white_pixel = 255;
     int image_size = img.height * img.step;
-    bool white_detected = false;
 
     float x_val, z_val = 0.0;
-    bool moving = false;
+    bool white_detected = false;
 
     for (int i = 0; i < image_size; i+=3){
-      if (img.data[i] == 255 && img.data[i+1] == 255){
-	    ROS_INFO_STREAM("px " << i);
+      if (img.data[i] == 255 && img.data[i+1] == 255 && img.data[i+2] == 255){
 	  white_detected = true;
-	  if (i%img.height < img.step*0.3){
+	  if (i%img.step < img.step/3){
 	   // go left
-    ROS_INFO_STREAM("LEFT...");
-	   x_val = 0.0;
-	   z_val = 0.5;
+	   x_val = 0.2;
+	   z_val = 0.2;
 	  }
-	  else if (i%img.height > img.step*0.6){
-//GO RIGHT
-            ROS_INFO_STREAM("RIGHT...");
-	   x_val = 0.5;
-	   z_val = 0.5;
+	  else if (i%img.step > img.step*0.6){
+	   //GO RIGHT
+	   x_val = 0.2;
+	   z_val = -0.2;
 	  }
           else {
-    ROS_INFO_STREAM("FWD.");
 	   x_val = 0.5;
 	   z_val = 0.0;
-	   drive_robot(x_val, z_val);
 	  }
-	//drive_robot(x_val, z_val);
+	drive_robot(x_val, z_val);
 	moving = true;
 	break;
 	}
-      if (moving) drive_robot(0.0, 0.0);
+      else {
+        if (moving) drive_robot(0.0, 0.0);
+        moving = false;
+      }
     }
 }
 
